19c61fa9123c659072bb1ff85bb6d61c36188c99
Helga <89998178494@mail.ru>
2022-07-03 21:59:16 +0300

    добавит список задач без кнопки очистить

---
+++ first/src/ToDoList/ToDoList.jsx
@@ -0,0 +1,22 @@
+import React, {useState} from "react";
+
+import {ToDoListButtons} from "./ToDoListButtons";
+import {ToDoListDisplay} from "./ToDoListDisplay";
+import {type} from "@testing-library/user-event/dist/type";
+
+export const ToDoList = () => {
+    const [items, setItems] = useState([{text:'test'}])
+    const addText = (text) => {
+        setItems([...items, { text: text } ])
+    }
+    const minusText = () => {
+
+    }
+    return (
+        <div>
+            <ToDoListDisplay items={items}/>
+            <ToDoListButtons addText={addText} minusText={minusText}/>
+        </div>
+    )
+
+}
