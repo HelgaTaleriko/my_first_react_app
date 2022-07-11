9d9ac27e1412a2cce4d5f0aa93de25e7bc36a228
Helga <89998178494@mail.ru>
2022-07-11 23:03:50 +0300

    feat(list): Добавит List

---
+++ first/src/List/List.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {ListButtons} from "./ListButtons"
+import {ListDisplay} from "./ListDisplay"
+
+export const List = () => {
+    return (
+        <div>
+            <ListDisplay/>
+            <ListButtons/>
+        </div>
+
+    )
+}
